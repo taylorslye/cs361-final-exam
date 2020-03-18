6a74b10eb464516e0993f8368ab8489e6e70ca01
Taylor Slye <slyet@oregonstate.edu>
2020-03-18 11:31:28 -0700

    fix whitespace on pannier and removed unnecessary comments

--- pannier.rb
+++ pannier.rb
@@ -1,34 +1,11 @@
-
-
 class Pannier
 
   def initialize
-    @price = 3 # Set price. Flat rate per rental
+    @price = 3 
   end
 
-
-  # Get price
   def price
     @price
   end
 
-  end
-
-
-
-  # def weight
-  #   @weight
-  # end
-
-
-
-
-
-
-
-
-
-
-
-
-
+end
