201e655d5dd60ac7482b503a2815ccbad874e639
Taylor Slye <slyet@oregonstate.edu>
2020-03-18 11:13:38 -0700

    added bike superclass

---
+++ bike.rb
@@ -0,0 +1,22 @@
+Class bike
+  
+  def luggage
+    @luggage
+  end
+
+  def weekly_rate
+    @weekly_rate
+  end
+
+  def daily_rate
+    @daily_rate
+  end
+
+  def hourly_rate
+    @hourly_rate
+  end
+
+
+
+
+end
--- mountain_bike.rb
+++ mountain_bike.rb
@@ -17,20 +17,4 @@ class MountainBike
     puts "Adjusting suspension..."
   end
 
-  def luggage
-    @luggage
-  end
-
-  def weekly_rate
-    @weekly_rate
-  end
-
-  def daily_rate
-    @daily_rate
-  end
-
-  def hourly_rate
-    @hourly_rate
-  end
-
 end
